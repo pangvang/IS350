ffd8f18b1661792235f1e7386c00f50131f28c9e
Pang Ter Vang <vangp65@uwosh.edu>
2020-12-18 19:55:17 -0600

    Added package.json, PORT changes to web server, procfile

--- hello.js
+++ hello.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var url = require('url');
 const PORT = process.env.PORT || 5000;
 
-
 http.createServer(function(req, res) {
 	var q = url.parse(req.url, true);
 	var filename = "." + q.pathname;
@@ -18,14 +17,14 @@ http.createServer(function(req, res) {
 
 	fs.readFile(filename, function(err, data) {
 		if (err){
-		res.writeHead(404, {'Content-Type': 'text/html'});
-		return res.end("404: Page Not Found");
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404: Page Not Found");
 		}
-		
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data);
 		return res.end();
 	});
 }).listen(PORT);
 
-console.log("Server Listening on assigned PORT...");
+console.log("Server Listening on assinged PORT...");
